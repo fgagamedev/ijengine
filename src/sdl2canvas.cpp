5db16959f89b2b9741508de325dca1e6591a5276
Mattioli <lucas.mattioli@live.com>
2016-04-14 12:20:00 -0300

    Implement SDL2Canvas' renderer method on its corresponding .cpp.

--- src/sdl2canvas.cpp
+++ src/sdl2canvas.cpp
@@ -5,28 +5,32 @@
 
 namespace ijengine {
 
-SDL2Canvas::SDL2Canvas(SDL_Renderer *renderer)
-    : m_renderer(renderer)
-{
-}
+    SDL2Canvas::SDL2Canvas(SDL_Renderer *renderer)
+        : m_renderer(renderer)
+    {
+    }
 
-void
-SDL2Canvas::draw(const Texture *texture, int x, int y)
-{
-    const SDL2Texture *text = dynamic_cast<const SDL2Texture *>(texture);
+    void
+    SDL2Canvas::draw(const Texture *texture, int x, int y)
+    {
+        const SDL2Texture *text = dynamic_cast<const SDL2Texture *>(texture);
 
-    SDL_SetRenderDrawColor(m_renderer, 255, 255, 255, 0);
-    SDL_RenderClear(m_renderer);
+        SDL_SetRenderDrawColor(m_renderer, 255, 255, 255, 0);
+        SDL_RenderClear(m_renderer);
 
-    SDL_Rect rect { x, y, text->w(), text->h() };
-    SDL_RenderCopy(m_renderer, text->texture(), nullptr, nullptr);
-    SDL_RenderPresent(m_renderer);
-}
+        SDL_Rect rect { x, y, text->w(), text->h() };
+        SDL_RenderCopy(m_renderer, text->texture(), nullptr, &rect);
+        SDL_RenderPresent(m_renderer);
+    }
 
-void
-SDL2Canvas::update()
-{
-    SDL_RenderPresent(m_renderer);
-}
+    SDL_Renderer *
+    SDL2Canvas::renderer() const {
+        return m_renderer;
+    }
 
+    void
+    SDL2Canvas::update()
+    {
+        SDL_RenderPresent(m_renderer);
+    }
 }
